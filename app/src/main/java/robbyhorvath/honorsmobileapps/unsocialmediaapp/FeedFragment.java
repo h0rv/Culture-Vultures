42a73c25816330e75188e089a11e322aea0f76ac
Robby Horvath <horvathrobby@gmail.com>
2020-05-05 15:01:56 -0400

    Add Bottom Navigation Bar Functionality

--- app/src/main/java/robbyhorvath/honorsmobileapps/unsocialmediaapp/FeedFragment.java
+++ app/src/main/java/robbyhorvath/honorsmobileapps/unsocialmediaapp/FeedFragment.java
@@ -1,13 +1,11 @@
 package robbyhorvath.honorsmobileapps.unsocialmediaapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
 
 public class FeedFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
--- app/src/main/java/robbyhorvath/honorsmobileapps/unsocialmediaapp/MainActivity.java
+++ app/src/main/java/robbyhorvath/honorsmobileapps/unsocialmediaapp/MainActivity.java
@@ -1,12 +1,12 @@
 package robbyhorvath.honorsmobileapps.unsocialmediaapp;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
-
-import android.os.Bundle;
-import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -18,33 +18,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         bottomNavigationView = findViewById(R.id.navigation);
-        setBottomNavigationListener();
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        openFragment(FeedFragment.newInstance());
     }
 
 
-    private void setBottomNavigationListener() {
-        BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.action_feed:
-                        openFragment(FeedFragment.newInstance());
-                        return true;
-                    case R.id.action_profile:
-                        openFragment(ProfileFragment.newInstance());
-                        return true;
-                    case R.id.action_search:
-                        openFragment(SearchFragment.newInstance());
-                        return true;
+    BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.action_feed:
+                            openFragment(FeedFragment.newInstance());
+                            return true;
+                        case R.id.action_profile:
+                            openFragment(ProfileFragment.newInstance());
+                            return true;
+                        case R.id.action_search:
+                            openFragment(SearchFragment.newInstance());
+                            return true;
+                    }
+                    return false;
                 }
-                return false;
-            }
-        };
-    }
+            };
+
 
     public void openFragment(Fragment fragment) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_holder, fragment);
+        transaction.addToBackStack(null);
         transaction.commit();
+
     }
 }

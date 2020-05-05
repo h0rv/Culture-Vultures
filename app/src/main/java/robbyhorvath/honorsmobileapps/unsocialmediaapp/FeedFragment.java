34126338bb314abd0951fd80aa8c71ba70b33233
Robby Horvath <horvathrobby@gmail.com>
2020-05-05 14:36:56 -0400

    Add Fragment classes and layout files

---
+++ app/src/main/java/robbyhorvath/honorsmobileapps/unsocialmediaapp/FeedFragment.java
@@ -0,0 +1,58 @@
+package robbyhorvath.honorsmobileapps.unsocialmediaapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FeedFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public FeedFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment FeedFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static FeedFragment newInstance() {
+        FeedFragment fragment = new FeedFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+         View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
+        // Inflate the layout for this fragment
+        return rootView;
+    }
+}
--- app/src/main/java/robbyhorvath/honorsmobileapps/unsocialmediaapp/MainActivity.java
+++ app/src/main/java/robbyhorvath/honorsmobileapps/unsocialmediaapp/MainActivity.java
@@ -1,14 +1,50 @@
 package robbyhorvath.honorsmobileapps.unsocialmediaapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.navigation);
+        setBottomNavigationListener();
+    }
+
+
+    private void setBottomNavigationListener() {
+        BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_feed:
+                        openFragment(FeedFragment.newInstance());
+                        return true;
+                    case R.id.action_profile:
+                        openFragment(ProfileFragment.newInstance());
+                        return true;
+                    case R.id.action_search:
+                        openFragment(SearchFragment.newInstance());
+                        return true;
+                }
+                return false;
+            }
+        };
+    }
+
+    public void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_holder, fragment);
+        transaction.commit();
     }
 }
